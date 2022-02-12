e15a75342ee4795a24736e5499effba296deae2b
Will Tengyuan Li <will19940430@gmail.com>
2022-02-11 19:23:13 -0700

    added html and js files

---
+++ client.js
@@ -0,0 +1,14 @@
+// before
+let city = "Calgary";
+console.log('I am from',city+'.');
+let time = 7 ;
+console.log('I have been in Canada for',time,'years.');
+let bornHere = false;
+console.log('Are you born in Canada?',bornHere);
+let hobbies = ['badminton','cooking','traveling'];
+console.log('my hobbies are',hobbies);
+// after
+console.log('Next year will be the',++time+'th year in Canada.');
+console.log('I mentioned',hobbies.length,'hobbies today.');
+console.log(`I am living in ${city},I have been in Canada for ${--time}years.`);
+console.log('Do you like Canada?',!bornHere);
